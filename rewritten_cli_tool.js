db2125d4d6631e6d256ef0c6814ca72d99465f7b
human2l <human2lkai@gmail.com>
2020-02-19 10:33:38 +1100

    finished basic realtime mode function. setup stepping mode UI

--- rewritten_cli_tool.js
+++ rewritten_cli_tool.js
@@ -1,3 +1,4 @@
+const messageContent = document.getElementById("messageContent");
 const testContent = document.getElementsByClassName("test")[0];
 const controlPanel = document.getElementsByClassName("control-panel")[0];
 const controlPanelPlaceholder = document.getElementsByClassName(
@@ -6,6 +7,10 @@ const controlPanelPlaceholder = document.getElementsByClassName(
 const contentPlaceholder = document.getElementsByClassName(
   "content-placeholder"
 )[0];
+const parameterArea = document.getElementById("parameter-area");
+const parameterAreaPlaceholder = document.getElementById(
+  "parameter-area-placeholder"
+);
 const content = document.getElementsByClassName("content")[0];
 const startBtn = document.getElementById("startBtn");
 const pauseBtn = document.getElementById("pauseBtn");
@@ -14,96 +19,54 @@ const inputFileElement = document.getElementById("fileInput");
 const video = document.getElementById("inputVideoSrc");
 const uploadCanvas = document.getElementById("uploadCanvas");
 const uploadCtx = uploadCanvas.getContext("2d");
-// const svgShowCase = document.getElementsByClassName("svg-showcase")[0];
+const endpointInput = document.getElementById("endpoint");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const typeInput = document.getElementById("type");
 const borderCanvasArea = document.getElementById("border-canvas-area");
-let ws = undefined;
-let cap = undefined;
-let frame = undefined;
-let imageScaleRatio = 1 / 4;
 const showcaseCanvas = document.getElementById("showcaseCanvas");
 const showcaseCtx = showcaseCanvas.getContext("2d");
 
+let ws = undefined;
+let cap = undefined;
+let frame = undefined;
+let imageScaleRatio = 1;
+let videoIsProcessing = false;
+let processingInterval = undefined;
+let ignoreMessageReceived = false;
+let mode = "realtimeMode";
+
 init();
-initConnection();
-initInputCanvas();
 
 function init() {
+  initDefaultConfiguration();
+  initControlDisplayState();
+  initConnectionSettings();
+  initInputCanvas();
+}
+
+function initControlDisplayState() {
   controlPanel.hidden = true;
   content.hidden = true;
   contentPlaceholder.hidden = true;
+  pauseBtn.hidden = true;
+  parameterArea.hidden = true;
+  parameterAreaPlaceholder.hidden = false;
 }
 
-function startAndSend() {
-  startBtn.disabled = true;
-  processVideo();
+function initDefaultConfiguration() {
+  imageScaleRatio = 1 / 4;
+  mode = "realtimeMode";
 }
 
-function pause() {
-  if (!video.paused) {
-    video.pause();
-    pauseBtn.innerHTML = "Resume";
-  } else {
-    processVideo();
-    pauseBtn.innerHTML = "Pause";
-  }
+function initConnectionSettings() {
+  endpointInput.value = "ws://192.168.1.209:8080";
+  usernameInput.value = "kai";
+  passwordInput.value = "anything";
+  typeInput.value = "whiteboard";
 }
 
-function reset() {
-  video.pause();
-  video.currentTime = 0;
-  uploadCtx.clearRect(0, 0, uploadCanvas.width, uploadCanvas.height);
-  showcaseCtx.clearRect(0, 0, showcaseCanvas.width, showcaseCanvas.height);
-}
-
-function initConnection() {
-  ws = new WebSocket(
-    // "wss://aminaiee@gmail.com:JIBB1234!@ipsa.testing.jibb.cloud/"
-    // "ws://localhost:9009/"
-    // "ws://kai:JIBB1234!@192.168.1.209:8080"
-    "ws://192.168.1.209:8080?username=kai&password=anything"
-    // "ws://ipsa.testing.jibb.cloud?username=kai&password=anything"
-  );
-
-  ws.onopen = function(evt) {
-    console.log("Connection open ...");
-  };
-
-  ws.onmessage = function(evt) {
-    console.log("Received Message: " + evt.data);
-    startBtn.disabled = false;
-    var evtObject = JSON.parse(evt.data);
-    // console.log(evtObject);
-
-    //TODO: add handle warning & error message
-
-    //need to modify to a more logical one
-    if (evtObject[2] != undefined) {
-      var svgObject = evtObject[2];
-      handleSVG(svgObject);
-      var cornerObject = evtObject[3];
-      handleCorners(cornerObject);
-    }
-  };
-
-  ws.onclose = function(evt) {
-    console.log(evt);
-    console.log("Connection closed.");
-  };
-  ws.onerror = function(evt) {
-    console.log(evt);
-  };
-}
-
-function onOpenCvReady() {
-  console.log("opencv.js is ready");
-  cv["onRuntimeInitialized"] = () => {
-    //do something only after ready
-    controlPanelPlaceholder.hidden = true;
-    controlPanel.hidden = false;
-  };
-}
-
-//1920 2080
+//1920 2080    required:1920 1080
 function initInputCanvas() {
   inputFileElement.addEventListener(
     "change",
@@ -111,7 +74,8 @@ function initInputCanvas() {
       content.hidden = true;
       contentPlaceholder.hidden = false;
       video.src = URL.createObjectURL(e.target.files[0]);
-      setTimeout(() => {
+      video.oncanplay = function() {
+        video.muted = true;
         video.height = video.videoHeight * imageScaleRatio;
         video.width = video.videoWidth * imageScaleRatio;
         startBtn.disabled = false;
@@ -120,7 +84,7 @@ function initInputCanvas() {
         content.hidden = false;
         contentPlaceholder.hidden = true;
         initVideoCapture();
-      }, 3000);
+      };
     },
     false
   );
@@ -131,32 +95,174 @@ function initVideoCapture() {
   frame = new cv.Mat(video.height, video.width, cv.CV_8UC4);
 }
 
+//This function is used when the svg content is shifted.
+//It will send the first several frames slowly to give the server sometime to handle the input
+var preparationInterval = undefined;
+function preparation() {
+  preparationInterval = setInterval(() => {
+    processVideo();
+  }, 2000);
+}
+//End the preparation Interval
+function endPreparation() {
+  clearInterval(preparationInterval);
+}
+
+//startBtn click event
+function startAndSend() {
+    //for realtime mode
+  if (mode == "realtimeMode") {
+    startBtn.hidden = true;
+    pauseBtn.hidden = false;
+    video.play();
+    startProcessingInterval();
+    //for stepping mode
+  } else if (mode == "steppingMode") {
+
+  }
+}
+
+//pauseBtn click event
+function pause() {
+    //for realtime mode
+  if (mode == "realtimeMode") {
+    if (!video.paused) {
+      video.pause();
+      pauseBtn.innerHTML = "Resume";
+    } else {
+      video.play();
+      startProcessingInterval();
+      pauseBtn.innerHTML = "Pause";
+    }
+    //for stepping mode
+  } else if (mode == "steppingMode") {
+  }
+}
+
+//resetBtn click event
+function reset() {
+    //for realtime mode 
+  if (mode == "realtimeMode") {
+    ignoreMessageReceived = true;
+    pauseBtn.hidden = true;
+    startBtn.hidden = false;
+    video.pause();
+    video.currentTime = 0;
+    if (processingInterval != undefined) clearInterval(processingInterval);
+    uploadCtx.clearRect(0, 0, uploadCanvas.width, uploadCanvas.height);
+    showcaseCtx.clearRect(0, 0, showcaseCanvas.width, showcaseCanvas.height);
+    if (borderCanvasArea.hasChildNodes()) {
+      borderCanvasArea.removeChild(borderCanvasArea.childNodes[0]);
+    }
+    ignoreMessageReceived = false;
+    //for stepping mode 
+  } else if (mode == "steppingMode") {
+
+  }
+}
+
+function buildConnection() {
+  if (ws != undefined) {
+    ws.close();
+  }
+  ws = new WebSocket(
+    endpointInput.value +
+      "?username=" +
+      usernameInput.value +
+      "&password=" +
+      passwordInput.value
+  );
+
+  ws.onopen = function(evt) {
+    console.log("Connection open ...");
+  };
+
+  ws.onmessage = function(evt) {
+    console.log(evt.data);
+    if (ignoreMessageReceived) return;
+    messageContent.innerHTML = "";
+    var evtObject = JSON.parse(evt.data);
+    var messageArray = [];
+    for (var index in evtObject) {
+      var currentObject = evtObject[index];
+      var firstKey = Object.keys(currentObject)[0];
+      switch (firstKey) {
+        case "version":
+          messageContent.innerHTML = "Connected to IPSA!";
+          //   console.log(currentObject);
+          break;
+        case "error":
+          var errorMessage = currentObject[firstKey];
+          messageArray.push("Error: " + errorMessage);
+          break;
+        case "warning":
+          var warningMessage = currentObject[firstKey];
+          messageArray.push("Warning: " + warningMessage);
+          break;
+        case "processing_time":
+          videoIsProcessing = false;
+          // console.log(currentObject);
+          break;
+        case "svg":
+          handleSVG(currentObject);
+          break;
+        case "corners":
+          handleCorners(currentObject);
+          break;
+        default:
+          break;
+      }
+    }
+    var message = "";
+    for (var index in messageArray) {
+      message += messageArray[index] + ". ";
+      message += "<br />";
+    }
+    messageContent.innerHTML += message;
+  };
+
+  ws.onclose = function(evt) {
+    console.log("Connection closed.");
+    messageContent.innerHTML = "Connection closed!";
+  };
+  ws.onerror = function(evt) {
+    console.log(evt);
+  };
+}
+
+//When opencv.js loaded successfully
+function onOpenCvReady() {
+  messageContent.innerHTML = "opencv.js is ready!";
+  cv["onRuntimeInitialized"] = () => {
+    controlPanelPlaceholder.hidden = true;
+    controlPanel.hidden = false;
+  };
+}
+
+//Note: the limit of server to handle the jpg is around 1.5 second.
+//When keep sending the jpg file too frequently, the svg content will shift (Same as the bug testing team found)
+function startProcessingInterval() {
+  processingInterval = setInterval(() => {
+    processVideo();
+    if (video.paused) clearInterval(processingInterval);
+  }, 1500);
+}
+
 function processVideo() {
-  if (video.paused) video.play();
+  if (inputFileElement.files.item(0) == null) return;
+  if (videoIsProcessing) return;
+  videoIsProcessing = true;
   cap.read(frame);
   cv.imshow("uploadCanvas", frame);
   var imgURI = uploadCanvas.toDataURL("image/jpeg");
-  var bytesarray = convertBase64ToBytearray(imgURI);
+  var bytesarray = Utils.convertBase64ToBytearray(imgURI);
   ws.send(bytesarray);
 }
 
-const convertBase64ToBytearray = function(image) {
-  const byteString = atob(image.split(",")[1]);
-  const ab = new ArrayBuffer(byteString.length);
-  const ia = new Uint8Array(ab);
-  for (let i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i);
-  }
-  return ia;
-};
-
+//show svg image through showcaseCanvas
 function handleSVG(svgObject) {
   var svgString = svgObject.svg;
   var svgNode = Utils.createElementFromHTML(svgString);
-  //for testing:
-  //   svgShowCase.replaceChild(svgNode, svgShowCase.childNodes[0]);
-  //   showcaseCanvas.width = svgNode.getAttribute("width") * imageScaleRatio;
-  //   showcaseCanvas.height = svgNode.getAttribute("height") * imageScaleRatio;
   showcaseCanvas.width = video.width;
   showcaseCanvas.height = video.height;
   var context = showcaseCanvas.getContext("2d");
@@ -164,6 +270,7 @@ function handleSVG(svgObject) {
   Utils.importSVG(svgNode, showcaseCanvas);
 }
 
+//show corners through cornerCanvas
 function handleCorners(cornerObject) {
   var borderCanvas = Utils.cloneCanvas(uploadCanvas);
   var borderCtx = borderCanvas.getContext("2d");
@@ -179,43 +286,18 @@ function handleCorners(cornerObject) {
   }
   borderCtx.strokeStyle = "red";
   borderCtx.stroke();
-  borderCanvasArea.replaceChild(borderCanvas, borderCanvasArea.childNodes[0]);
+  if (borderCanvasArea.hasChildNodes())
+    borderCanvasArea.replaceChild(borderCanvas, borderCanvasArea.childNodes[0]);
+  else borderCanvasArea.appendChild(borderCanvas);
 }
 
-Utils = {
-  createElementFromHTML: function(htmlString) {
-    var div = document.createElement("div");
-    div.innerHTML = htmlString.trim();
-
-    // Change this to div.childNodes to support multiple top-level nodes
-    return div.firstChild;
-  },
-
-  importSVG: function(sourceSVG, targetCanvas) {
-    svg_xml = new XMLSerializer().serializeToString(sourceSVG);
-    var ctx = targetCanvas.getContext("2d");
-    var img = new Image();
-    img.src = "data:image/svg+xml;base64," + btoa(svg_xml);
-
-    img.onload = function() {
-      ctx.drawImage(img, 0, 0, targetCanvas.width, targetCanvas.height);
-    };
-  },
-
-  cloneCanvas: function(oldCanvas) {
-    var newCanvas = document.createElement("canvas");
-    var context = newCanvas.getContext("2d");
-    newCanvas.width = oldCanvas.width;
-    newCanvas.height = oldCanvas.height;
-    context.drawImage(oldCanvas, 0, 0);
-    return newCanvas;
+function toggleMode(e) {
+  mode = e.id;
+  if (mode == "realtimeMode") {
+    parameterArea.hidden = true;
+    parameterAreaPlaceholder.hidden = false;
+  } else if (mode == "steppingMode") {
+    parameterArea.hidden = false;
+    parameterAreaPlaceholder.hidden = true;
   }
-};
-
-function showSize() {
-  console.log(video.height + "video w: " + video.width);
-  console.log(uploadCanvas.height + "uploadCanvas w:" + uploadCanvas.width);
-  console.log(
-    showcaseCanvas.height + "showcaseCanvas w:" + showcaseCanvas.width
-  );
 }
---
+++ utils.js
@@ -0,0 +1,44 @@
+Utils = {
+//   $: function(x) {
+//     return document.getElementById(x);
+//   },
+
+  createElementFromHTML: function(htmlString) {
+    var div = document.createElement("div");
+    div.innerHTML = htmlString.trim();
+
+    // Change this to div.childNodes to support multiple top-level nodes
+    return div.firstChild;
+  },
+
+  //param: image -- type:imageURI
+  convertBase64ToBytearray: function(image) {
+    const byteString = atob(image.split(",")[1]);
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    return ia;
+  },
+
+  importSVG: function(sourceSVG, targetCanvas) {
+    svg_xml = new XMLSerializer().serializeToString(sourceSVG);
+    var ctx = targetCanvas.getContext("2d");
+    var img = new Image();
+    img.src = "data:image/svg+xml;base64," + btoa(svg_xml);
+
+    img.onload = function() {
+      ctx.drawImage(img, 0, 0, targetCanvas.width, targetCanvas.height);
+    };
+  },
+
+  cloneCanvas: function(oldCanvas) {
+    var newCanvas = document.createElement("canvas");
+    var context = newCanvas.getContext("2d");
+    newCanvas.width = oldCanvas.width;
+    newCanvas.height = oldCanvas.height;
+    context.drawImage(oldCanvas, 0, 0);
+    return newCanvas;
+  }
+};
